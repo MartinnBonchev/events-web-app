86beedbcae23b5eb11162e4c7e3cbc7636b403ac
Martin Bonchev <martin.bonchevv@gmail.com>
2023-05-01 15:58:58 +0300

    feat: implement app index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,49 @@
+import { useNavigate } from "react-router-dom";
+
+import Event from "@root/features/event";
+import Button from "@components/button";
+import Search from "@features/search";
+import { useAppSelector } from "@store/hooks";
+import {
+  selectEvents,
+  selectFilteredEvents,
+} from "@store/reducers/events/events.selectors";
 
 function App() {
-  return <button>hello</button>;
+  const events = useAppSelector(selectEvents);
+  const filteredEvents = useAppSelector(selectFilteredEvents);
+  const navigate = useNavigate();
+
+  const handleNavigateWishlist = () => navigate("/wishlist");
+
+  const handleNavigateCreateEvent = () => navigate("/create-event");
+
+  return (
+    <div id="container">
+      <div>
+        <Button onClick={handleNavigateWishlist}>Wishlist</Button>
+
+        <Button onClick={handleNavigateCreateEvent}>Create Event</Button>
+      </div>
+
+      <Search />
+
+      {(filteredEvents.length ? filteredEvents : events).map(
+        ({ date, description, id, title, price, image, location }) => (
+          <Event
+            key={id}
+            id={id}
+            date={date}
+            description={description}
+            image={image}
+            title={title}
+            location={location}
+            price={price}
+          />
+        )
+      )}
+    </div>
+  );
 }
 
 export default App;
