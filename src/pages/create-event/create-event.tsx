1dc065342a5f6025536ba51f4568d58c06dde5e0
Martin Bonchev <martin.bonchevv@gmail.com>
2023-05-01 15:58:38 +0300

    feat: implement create event page

---
+++ src/pages/create-event/create-event.tsx
@@ -0,0 +1,11 @@
+import CreateEventForm from "@features/create-event-form";
+
+export default function CreateEvent() {
+  return (
+    <div>
+      <h1>Create Event</h1>
+
+      <CreateEventForm />
+    </div>
+  );
+}
