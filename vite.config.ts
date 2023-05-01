be6374131d19d02372f93669417ca78de63db3dd
Martin Bonchev <martin.bonchevv@gmail.com>
2023-05-01 15:54:55 +0300

    feat: aliases and change of ports

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
   resolve: {
     alias: {
       "@root": new URL("./src/", import.meta.url).pathname,
+      "@components": new URL("./src/components", import.meta.url).pathname,
+      "@features": new URL("./src/features", import.meta.url).pathname,
       "@pages": new URL("./src/pages", import.meta.url).pathname,
+      "@utils": new URL("./src/utils", import.meta.url).pathname,
       "@routes": new URL("./src/routes", import.meta.url).pathname,
       "@store": new URL("./src/store", import.meta.url).pathname,
     },
